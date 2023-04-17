8a456074725d2f908c3604b39eb77fdd5ba6b923
vincent porte <vincent@neuralia.co>
2023-04-18 01:03:18 +0200

    feat(users): use dependencies in route, manage integrityerror on create route ,add update and delete routes

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,16 +1,18 @@
-from fastapi import APIRouter, Depends
+from typing import Dict, Mapping
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.services.database import get_db
+from app.users.dependencies import valid_user_id
 from app.users.models import User as UserModel
-from app.users.schemas import UserSchema, UserSchemaCreate
+from app.users.schemas import UserSchema, UserSchemaCreate, UserSchemaUpdate
 
 router = APIRouter(prefix="/users")
 
 
-@router.get("/{id}/", response_model=UserSchema)
-async def get_user(id: str, db: AsyncSession = Depends(get_db)):
-    user = await UserModel.get(db, id)
+@router.get("/{user_id}/", response_model=UserSchema)
+async def get_user(user: Mapping = Depends(valid_user_id)):
     return user
 
 
@@ -25,4 +27,23 @@ async def get_users(db: AsyncSession = Depends(get_db)):
 @router.post("/create/", response_model=UserSchema)
 async def create_user(user: UserSchemaCreate, db: AsyncSession = Depends(get_db)):
     user = await UserModel.create(db, **user.dict())
+    if not user:
+        raise HTTPException(status_code=400, detail="User already exists")
     return user
+
+
+@router.patch("/{user_id}/", response_model=UserSchema)
+async def update_user(
+    user_datas: UserSchemaUpdate, user: UserSchema = Depends(valid_user_id), db: AsyncSession = Depends(get_db)
+):
+    updated_data = user_datas.dict(exclude_unset=True)
+    user = await UserModel.update(db, user, **updated_data)
+    if not user:
+        raise HTTPException(status_code=400, detail="Email already exists")
+    return user
+
+
+@router.delete("/{user_id}/", response_model=Dict[str, str])
+async def delete_user(user: dict = Depends(valid_user_id), db: AsyncSession = Depends(get_db)):
+    res = await UserModel.delete(user, db)
+    return {"message": f"User {res} deleted successfully"}
--- tests/users/test_routes.py
+++ tests/users/test_routes.py
@@ -1,3 +1,10 @@
+from faker import Faker
+
+from tests.fixtures import user, users  # noqa F401
+
+faker = Faker()
+
+
 def test_create_user(client):
     response = client.get("/api/users/")
     assert response.status_code == 200
@@ -11,14 +18,64 @@ def test_create_user(client):
     assert response.json().get("email") == "test@example.com"
     assert response.json().get("full_name") == "Full Name Test"
 
-    response = client.get(f"/api/users/{response.json().get('id')}")
-    assert response.status_code == 200
-    assert response.json() == {
-        "id": response.json().get("id"),
-        "email": "test@example.com",
-        "full_name": "Full Name Test",
-    }
-
+    # verify that the user is added to the database
     response = client.get("/api/users/")
     assert response.status_code == 200
     assert len(response.json()) == 1
+
+
+def test_create_user_with_duplicated_email(client, user):  # noqa F811
+    response = client.post(
+        "/api/users/create/",
+        json={"email": user.email, "full_name": "Full Name Test"},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "User already exists"}
+
+
+def test_get_user(client, user):  # noqa F811
+    response = client.get(f"/api/users/{user.id}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": user.id,
+        "email": user.email,
+        "full_name": user.full_name,
+    }
+
+
+def test_get_user_with_notuuid(client):
+    response = client.get("/api/users/notuuid")
+    assert response.status_code == 400
+
+
+def test_get_user_with_unexistant_id(client):
+    uuid = faker.uuid4()
+    response = client.get(f"/api/users/{uuid}")
+    assert response.status_code == 404
+
+
+def test_update_user(client, user):  # noqa F811
+    email = faker.email()
+    full_name = faker.name()
+    response = client.patch(f"/api/users/{user.id}", json={"email": email, "full_name": full_name})
+    assert response.status_code == 200
+    assert response.json() == {
+        "email": email,
+        "full_name": full_name,
+        "id": user.id,
+    }
+
+
+def test_update_user_with_duplicated_email(client, users):  # noqa F811
+    response = client.patch(f"/api/users/{users[0].id}", json={"email": users[1].email})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Email already exists"}
+
+
+def test_delete_user(client, user):  # noqa F811
+    response = client.delete(f"/api/users/{user.id}")
+    assert response.status_code == 200
+    assert response.json() == {"message": f"User {user.email} deleted successfully"}
+
+    response = client.delete(f"/api/users/{user.id}")
+    assert response.status_code == 404
