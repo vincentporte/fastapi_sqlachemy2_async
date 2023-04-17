6dff7840b7893b377960d9d0e56927336d90d57a
vincent porte <vincent@neuralia.co>
2023-04-17 19:13:40 +0200

    feat(users): add views, template and tests for user creation

--- app/users/views.py
+++ app/users/views.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Depends, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import ValidationError
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.datastructures import FormData
 
 from app.services.database import get_db
+from app.services.utils import get_errors_dict
 from app.users.models import User as UserModel
+from app.users.schemas import UserSchemaCreate
 
 templates = Jinja2Templates(directory="app/templates")
 router = APIRouter(prefix="/users")
@@ -15,3 +19,25 @@ async def get_users_list(request: Request, db: AsyncSession = Depends(get_db)):
     users = await UserModel.get_all(db)
     context = {"request": request, "users": users}
     return templates.TemplateResponse("users/list.html", context)
+
+
+@router.get("/create/", response_class=HTMLResponse)
+async def create_user_init(request: Request):
+    form_data: FormData = await request.form()
+    return templates.TemplateResponse("users/form.html", {"request": request, "form_data": form_data})
+
+
+@router.post("/create/", response_class=HTMLResponse)
+async def create_user_submit(request: Request, db: AsyncSession = Depends(get_db)):
+    form_data: FormData = await request.form()
+
+    try:
+        UserSchemaCreate(**form_data)
+        await UserModel.create(db, **form_data)
+        return RedirectResponse("/users/", status_code=302)
+
+    except ValidationError as e:
+        # TODO vincentporte : solve mypy error
+        errors = get_errors_dict(e.errors())  # type: ignore
+        context = {"request": request, "form_data": form_data, "errors": errors}
+        return templates.TemplateResponse("users/form.html", context)
--- tests/users/test_views.py
+++ tests/users/test_views.py
@@ -1,4 +1,8 @@
+from faker import Faker
+
 from tests.fixtures import users  # noqa F401
+
+fake = Faker()
 
 
 def test_users_list(client, users):  # noqa F811
@@ -7,3 +11,36 @@ def test_users_list(client, users):  # noqa F811
     assert response.template.name == "users/list.html"
     assert "request" in response.context
     assert "users" in response.context
+
+
+def test_user_create_init(client):
+    response = client.get("/users/create/")
+    assert response.status_code == 200
+    assert response.template.name == "users/form.html"
+    assert "request" in response.context
+    assert "form_data" in response.context
+    assert 'id="email"' in response.content.decode("utf-8")
+    assert 'id="full_name"' in response.content.decode("utf-8")
+
+
+def test_user_create_valid_submit(client):
+    data = {"full_name": fake.name(), "email": fake.email()}
+    response = client.post("/users/create/", data=data)
+    assert response.status_code == 200
+    assert response.url == "http://testserver/users/"
+
+    response = client.get("/users/")
+    assert response.status_code == 200
+    assert "users" in response.context
+    assert len(response.context["users"]) == 1
+    assert response.context["users"][0].email == data["email"]
+    assert response.context["users"][0].full_name == data["full_name"]
+
+
+def test_user_create_invalid_submit(client):
+    data = {"full_name": fake.name(), "email": "invalid_email"}
+    response = client.post("/users/create/", data=data)
+    assert response.status_code == 200
+    assert response.url == "http://testserver/users/create/"
+    assert {"email": "value is not a valid email address"} == response.context["errors"]
+    assert data == dict(response.context["form_data"])
